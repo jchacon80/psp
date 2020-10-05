c3f09f208b0d147064da860f5da03c46ec4e6b7a
Jorge Rodríguez <jchacon80@gmail.com>
2020-10-05 19:39:43 +0200

    modificación cmd dir

--- src/ejemplos/ud01/ProcessCommunication.java
+++ src/ejemplos/ud01/ProcessCommunication.java
@@ -4,22 +4,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class ProcessCommunication {
 
 	public static void main(String[] args) throws IOException {
 		
-		Process process = new ProcessBuilder(args).start();
+		final String commands[] = {"cmd", "/C", "dir"};
+		
+		Process process = new ProcessBuilder(commands).start();
 		InputStream is = process.getInputStream();
 		InputStreamReader isr = new InputStreamReader(is);
 		BufferedReader br = new BufferedReader(isr);
 		String line;
 		
-		System.out.println("Salida del proceso " + Arrays.toString(args) + ":");
-		
 		while ((line = br.readLine()) != null) {
 			System.out.println(line);
+		}
+		
+		// Esperamos que el proceso termine
+		try {
+			int exitValue = process.waitFor();
+			System.out.println("\nCódigo de salida: "+ exitValue);
+		} catch (InterruptedException e) {
+			e.printStackTrace(System.err);
+			Thread.currentThread().interrupt();
 		}
 	}
 }
